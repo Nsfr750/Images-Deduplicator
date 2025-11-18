19e655cad3b03ace23c5883700c45a39be676c10
Nsfr750 <nsfr750@gmail.com>
2025-11-18 16:47:56 +0100

    1.7.0

--- setup/firma.bat
+++
@@ -1,2 +0,0 @@
-X:\firma\signtool.exe sign /fd sha256 /td SHA256 /f "X:\Certificati\tuxxle.pfx" /p 01811365 /tr http://timestamp.digicert.com "X:\GitHub\Image-Deduplicator\dist\Image-Deduplicator.exe"
-pause
